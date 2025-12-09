f5298181d73935963ef48838443833401e7f9321
AngelicaSjodin <angelic.sjodin@gmail.com>
2025-12-09 03:43:28 +0100

    ännu mer fix i pom och application och dependencies

--- demo/src/main/java/com/example/demo/UserController.java
+++ demo/src/main/java/com/example/demo/UserController.java
@@ -1,12 +1,18 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import java.util.List;
 import com.example.demo.UserRepository;
 import com.example.demo.User;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/users")
+@CrossOrigin(origins = "http://localhost:8100")
 public class UserController {
   private final UserRepository repo;
   public UserController(UserRepository repo){ this.repo = repo; }
